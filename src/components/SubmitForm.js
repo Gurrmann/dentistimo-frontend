f60031d0fe345588ef7360c0cf3d12a0220483f5
Emil Gustafsson <emgusta@student.chalmers.se>
2020-11-21 16:58:24 +0100

    Add state handlers for form submission

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/SubmitForm.css';
 
-function SubmitForm () {
-    return(
-        <div>
-            <form>
-                <label>Name:</label><br/>
-                <input type="text" name="name" placeholder = 'Name Namerson'/><br/>
-                <label>Phone number:</label><br/>
-                <input type="text" phoneNumber="phone" placeholder = '0713333337'/><br/>
-                <label>Email:</label><br/>
-                <input type="text" eMail="mail" placeholder = 'youremail@domain.gov' /><br/>
-                <label>Time:</label><br/>
-                (Selected Time Slot Goes Here)<br/>
-                <label>Dentistry:</label><br/>
-                (Selected Dentistry Goes Here)<br/>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    )
-}
+class SubmitForm extends Component {
+    constructor(props){
+        super(props)
 
+        this.state = {
+            name: '',
+            phoneNumber: '',
+            eMail: '',
+            timeSlot: '',
+            dentistry: ''
+        }
+    }
+    handleNameChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    handlePhoneNumberChange = (event) => {
+        this.setState({
+            phoneNumber: event.target.value
+        })
+    }
+    handleEMailChange = (event) => {
+        this.setState({
+            eMail: event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        alert(`${this.state.name} ${this.state.phoneNumber} ${this.state.eMail} ${this.state.timeSlot} ${this.state.dentistry}`)
+        event.preventDefault()
+    }
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name:</label><br/>
+                    <input type="text" value={this.state.name} placeholder = 'Name Namerson' onChange={this.handleNameChange}/><br/>
+                    <label>Phone number:</label><br/>
+                    <input type="text" value={this.state.phoneNumber} placeholder = '0713333337' onChange={this.handlePhoneNumberChange}/><br/>
+                    <label>Email:</label><br/>
+                    <input type="text" value={this.state.eMail} placeholder = 'youremail@domain.gov' onChange={this.handleEMailChange} /><br/>
+                    <label>Time:</label><br/>
+                    {this.state.timeSlot}<br/>
+                    <label>Dentistry:</label><br/>
+                    {this.state.dentistry}<br/>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+}
 export default SubmitForm;
