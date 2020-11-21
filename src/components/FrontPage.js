ae7e41b13fafd77c296ddca79f053ac9382837d1
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-11-21 17:00:49 +0100

    Added placeholder for calendar

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import InfoText from './InfoText.js'
 import SubmitForm from './SubmitForm.js'
+import Calendar from './Calendar.js'
 import '../css/FrontPage.css'
 
 function FrontPage() {
@@ -8,6 +9,7 @@ function FrontPage() {
       <div >
         <InfoText className='background' />
         <SubmitForm />
+        <Calendar />
       </div>
 
 
