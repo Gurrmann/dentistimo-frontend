de6a63c0aba24b58ed07d613189d2add736998ce
Emil Gustafsson <emgusta@student.chalmers.se>
2020-11-21 01:23:36 +0100

    Submit Form clean up

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -5,34 +5,12 @@ function SubmitForm () {
     return(
         <div>
             <form>
-                <label>
-                    Name:
-                    <input type="text" name="name" placeholder = 'Name'/>
-                </label>
-                <br/>
-                <label>
-                    Phone number:
-                    <input type="text" phoneNumber="phone" placeholder = 'Phone number'/>
-                </label>
-                <br/>
-                <label>
-                    Email:
-                    <input type="text" eMail="mail" placeholder = 'Email' />
-                </label>
-                <br/>
-                <label>
-                    ssn:
-                    <input type="text" ssn="ssn" placeholder = 'SSN'/>
-                </label>
-                <br/>
-                <label>
-                    Time:
-                </label>
-                <br/>
-                <label>
-                    Selected Dentistry:
-                </label>
-                <br/>
+                <label>Name:<input type="text" name="name" placeholder = 'Name'/></label><br/>
+                <label>Phone number:<input type="text" phoneNumber="phone" placeholder = 'Phone number'/></label><br/>
+                <label>Email:<input type="text" eMail="mail" placeholder = 'Email' /></label><br/>
+                <label>ssn:<input type="text" ssn="ssn" placeholder = 'SSN'/></label><br/>
+                <label>Time:</label><br/>
+                <label>Selected Dentistry:</label><br/>
                 <input type="submit" value="Submit" />
             </form>
         </div>
