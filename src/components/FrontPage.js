2d7cb03a1713be5892a065e65cd7c6d2cfc427a7
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-11-22 17:51:47 +0100

    Moved infotext component to frontpage file

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import InfoText from './InfoText.js'
 import SubmitForm from './SubmitForm.js'
 import Calendar from './Calendar.js'
 import DenistryMap from './DenistryMap.js'
@@ -7,15 +6,42 @@ import '../css/FrontPage.css'
 
 function FrontPage() {
     return (
-      <div className='container-1'>
+      <>
         <InfoText />
-        <SubmitForm />
-        <Calendar />
-        <DenistryMap />
+      <div className='submission-container-1'>
+        <div className='box-1'>
+          <TextBlock />
+        </div>
+        <div className='box-2'>
+          <SubmitForm/>
+        </div>
       </div>
+        <div className='box-3'>
+          <Calendar />
+        </div>
+        <div className='box-4'>
+          <DenistryMap />
+        </div>
+      </>
 
 
     )
+}
+
+const InfoText = () => {
+  const banner = 'Dentistimo'
+    return (
+      <div>
+        <h1 id='infoText'>{banner}</h1>
+      </div>
+    )
+}
+
+const TextBlock = () => {
+  const textBlock = 'To book a time, either pick the dentistry you want from the form or select a marker on the map and then fill in your info and submit'
+  return (
+      <p id='textBlock'>{textBlock}</p>
+  )
 }
 
 export default FrontPage;
