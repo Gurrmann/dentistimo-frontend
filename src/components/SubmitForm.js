78407d5ab4b41126c6f63f71f6e7a62bf8b27807
simengs <simengs@student.chalmers.se>
2020-11-30 23:53:59 +0100

    Added method to divide up the openinghours into timeslots

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import '../css/SubmitForm.css';
 
+var timeSlotArr = []
+var dentistArr = []
+
 class SubmitForm extends Component {
     state = {
         selectedDentistryOption: 'None',
@@ -33,8 +36,58 @@ class SubmitForm extends Component {
     }
     handleDentistryChange = (event) => {
         this.setState({dentistry: event.target.value});
-        console.log(event.target.value)
+        timeSlotArr = []
+        var thisDentist = []
+        var selectedDay = 'monday'
+        var openingHours = ''
+        var start = ''
+        var end = ''
+
+        //Checks of there is a dentistry within the array that matches the name of the selected dentistry, if true it copies the dentistry
+        //over to thisDentist array
+        for (var i = 0; i < dentistArr.length; i++){
+            if (dentistArr[i].name === event.target.value){
+                thisDentist = dentistArr[i]
+            }
+        }
+        //Checks if which day of the week has been selected
+        for (var i = 0; i < Object.keys(thisDentist.openinghours).length; i++){
+            if (Object.keys(thisDentist.openinghours)[i] === selectedDay){
+                openingHours = Object.values(thisDentist.openinghours)[i]
+            }
+        }
+        //checks if start begins with a single digit or two
+        if (openingHours.charAt(1) === ':'){
+        start = openingHours.substring(0, 4)
+        end = openingHours.substring(5, 10)
+        } 
+        else {
+            start = openingHours.substring(0, 6)
+            end = openingHours.substring(6, 11)
+        }
+        start = start.replace(':', '.')
+        end = end.replace(':', '.')
+
+        //Parses start and end to floats so we can do math on them
+        start = parseFloat(start)
+        end = parseFloat(end)
+
+        //Calculate the total amount of hours that the dentistry will be open for the selected day
+        var totalHours = (end - start)*2
+        var halfHour = false
+
+        //Divides the openhours into timeslots
+        for (var i = 0; i < totalHours; i++){
+        if (halfHour){
+            timeSlotArr[i] = {time_slot: + '' + start + '' + ':30'}
+            
+            start++
+        }else {
+        timeSlotArr[i] = {time_slot: + '' + start + '' + ':00'}
+        }
+        halfHour = !halfHour
     }
+}
     handleTimeChange = (event) => {
         this.setState({timeSlot: event.target.value});
         console.log(event.target.value)
@@ -50,18 +103,15 @@ class SubmitForm extends Component {
     }
         
     render() {
-
-        
-        var dentistArr = []
-
  //The data from frontpage is sent after the webpage has loaded, so we check if it has been sent, if it has not we have an empty array
  // drop down options for timetable, not working {dentistArr.map(({monday, id}, index) => <option key={id} id ={id} >{monday}</option>)}
         if (this.props.dentistryarr.length) {
-            for (var i = 0; i < this.props.dentistryarr.length; i++){
-            dentistArr.push(this.props.dentistryarr[i])
+            if (dentistArr.length < this.props.dentistryarr.length){
+                for (var i = 0; i < this.props.dentistryarr.length; i++){
+                    dentistArr.push(this.props.dentistryarr[i])
+                }
+            }
         }
-    }
-
         return(
             <div id='position'>
               <TextBlock /> 
@@ -72,17 +122,15 @@ class SubmitForm extends Component {
                     <input type="text" value={this.state.phoneNumber} placeholder = '0712345678' onChange={this.handlePhoneNumberChange}/><br/>
                     <label>Email:</label><br/>
                     <input type="text" value={this.state.eMail} placeholder = 'youremail@domain.gov' onChange={this.handleEMailChange} /><br/>
-                     <label>Time:</label><br/>
+                     <label>Select a time</label><br/>
                     <select value= {this.state.timeSlot} onChange={this.handleTimeChange}>
-                        <option default>Select a time slot</option>
-                        <option>-------</option>
-                        {dentistArr.map(({name, id}, index) => <option key={id} id ={id} >{name}</option>)}  
+                        <option default disabled >Select a time slot</option>
+                        {timeSlotArr.map(({time_slot}, index) => <option key={time_slot} time_slot ={time_slot} >{time_slot}</option>)}  
                        
                     </select><br/>
-                    <label>Dentistry:</label><br/>
+                    <label>Select a dentistry</label><br/>
                     <select value= {this.state.dentistry} onChange={this.handleDentistryChange}>
-                        <option default>Select your dentistry</option>
-                        <option>-------</option>
+                        <option default disabled>Select your dentistry</option>
                         {dentistArr.map(({name, id}, index) => <option key={id} id ={id} >{name}</option>)}             
                     </select>
                     <br/>
