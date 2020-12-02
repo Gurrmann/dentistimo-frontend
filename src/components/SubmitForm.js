8de131722d954ab03f871a384ba5d2bbe5fbb732
simengs <simengs@student.chalmers.se>
2020-12-02 01:58:17 +0100

    Added method for saving the selected date from calender to variable, added method that updates timeSlotArr when a new date has been selected, added method to randomly generate userid

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -3,8 +3,11 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import '../css/SubmitForm.css';
 
-var timeSlotArr = []
-var dentistArr = []
+var dentistArr = []          //The array of the selected dentistry
+var selectedDate = ''        //Selected date YYYY-MM-DD
+var selectedDay = ''         //Selected day
+var selectedDentist = ''     //Name of selected dentist
+var selectedId = ''          //Id of the selected dentistry
 
 class SubmitForm extends Component {
     state = {
@@ -16,38 +19,50 @@ class SubmitForm extends Component {
         this.state = {
             timeSlot: '',
             dentistry: '',
+            timeSlotArr: []
         }
     }
     handleDentistryChange = (event) => {
-        this.setState({dentistry: event.target.value});
-        timeSlotArr = []
+        this.setState({dentistry: event.target.value,
+                        timeSlot: ''
+        });
+
+        selectedDentist = event.target.value
+        this.handleDentistry ()
+    }
+
+    handleDentistry () {    
+        if (selectedDentist === ''){
+        }else {
+            
+        this.setState({timeSlotArr: []})
         var thisDentist = []
-        var selectedDay = 'monday'
-        var openingHours = ''
+        var dayOfWeek = ''
         var start = ''
         var end = ''
 
         //Checks of there is a dentistry within the array that matches the name of the selected dentistry, if true it copies the dentistry
         //over to thisDentist array
         for (var i = 0; i < dentistArr.length; i++){
-            if (dentistArr[i].name === event.target.value){
+            if (dentistArr[i].name === selectedDentist){
                 thisDentist = dentistArr[i]
+                selectedId = dentistArr[i].id
             }
         }
         //Checks if which day of the week has been selected
         for (i = 0; i < Object.keys(thisDentist.openinghours).length; i++){
             if (Object.keys(thisDentist.openinghours)[i] === selectedDay){
-                openingHours = Object.values(thisDentist.openinghours)[i]
+                dayOfWeek = Object.values(thisDentist.openinghours)[i]
             }
         }
         //checks if start begins with a single digit or two
-        if (openingHours.charAt(1) === ':'){
-        start = openingHours.substring(0, 4)
-        end = openingHours.substring(5, 10)
+        if (dayOfWeek.charAt(1) === ':'){
+            start = dayOfWeek.substring(0, 4)
+            end = dayOfWeek.substring(5, 10)
         } 
         else {
-            start = openingHours.substring(0, 6)
-            end = openingHours.substring(6, 11)
+            start = dayOfWeek.substring(0, 6)
+            end = dayOfWeek.substring(6, 11)
         }
         start = start.replace(':', '.')
         end = end.replace(':', '.')
@@ -60,21 +75,23 @@ class SubmitForm extends Component {
         var totalHours = (end - start)*2
         var halfHour = false
 
+        var tempArr = []
         //Divides the openhours into timeslots
         for (i = 0; i < totalHours; i++){
         if (halfHour){
-            timeSlotArr[i] = {time_slot: + '' + start + '' + ':30'}
-            
+            tempArr.push({time_slot: + '' + start + '' + ':30'})
             start++
         }else {
-        timeSlotArr[i] = {time_slot: + '' + start + '' + ':00'}
+            tempArr.push({time_slot: + '' + start + '' + ':00'})
         }
         halfHour = !halfHour
-    }
+      }
+      this.setState({timeSlotArr: tempArr})
+  }
 }
+
     handleTimeChange = (event) => {
         this.setState({timeSlot: event.target.value});
-        console.log(event.target.value)
     }
     handleFormChange = ({ target }) => {
         this.setState({
@@ -84,9 +101,102 @@ class SubmitForm extends Component {
     handleSubmit = (event) => {
         var issuance = new Date()
         issuance = issuance.getTime()
-        alert(`${this.state.timeSlot} ${this.state.dentistry} ${issuance}`)
+        console.log(selectedDate)
+        var bookingRequest = {
+            
+            userid: Math.floor(Math.random() * 1000000000),
+        // requestid:
+            dentistid: selectedId,
+            issuance: issuance,
+            time: selectedDate + ' ' + this.state.timeSlot
+
+        }
+        console.log(bookingRequest)
         event.preventDefault()
     }
+    handleDateChange = (value, event) => {
+        this.setState({timeSlot: ''});
+        selectedDate = ''
+        var weekDay = ''
+        var month = ''
+        var day = ''
+        var year = ''
+        var selection = value.toString()
+        selection = selection.replace(/\s/g, "")
+
+        weekDay = selection.substring(0, 3)
+        month = selection.substring(3, 6)
+        day = year = selection.substring(6, 8)
+        year = selection.substring(8, 12)
+
+        switch(weekDay) {
+            case "Mon":
+                weekDay = "monday"
+            break;
+            case "Tue":
+                weekDay = "tuesday"
+            break;
+            case "Wed":
+                weekDay = "wednesday"
+            break;
+            case "Thu":
+                weekDay = "thursday"
+            break;
+            case "Fri":
+                weekDay = "friday"
+            break;
+            case "Sat":
+                weekDay = "saturday"
+            break;
+            case "Sun":
+                weekDay = "sunday"
+            break;
+          }
+          switch(month) {
+            case "Jan":
+                month = "01"
+            break;
+            case "Feb":
+                month = "02"
+            break;
+            case "Mar":
+                month = "03"
+            break;
+            case "Apr":
+                month = "04"
+            break;
+            case "May":
+                month = "05"
+            break;
+            case "Jun":
+                month = "06"
+            break;
+            case "Jul":
+                month = "07"
+            break;
+            case "Aug":
+                month = "08"
+            break;
+            case "Sep":
+                month = "09"
+            break;
+            case "Oct":
+                month = "10"
+            break;
+            case "Nov":
+                month = "11"
+            break;
+            case "Dec":
+                month = "12"
+            break;
+          }
+          
+          selectedDate = year + '-' + month + '-' + day
+          selectedDay = weekDay
+
+          this.handleDentistry()
+    }
+   
         
     render() {
  //The data from frontpage is sent after the webpage has loaded, so we check if it has been sent, if it has not we have an empty array
@@ -101,21 +211,20 @@ class SubmitForm extends Component {
         return(
             <div id='position'>
               <TextBlock/>
-              <label>Select a date:</label><br/>
-              <div><Calendar/></div>
+              <div><Calendar onChange={this.handleDateChange} Days={this.state.date} /></div>
                 <form onSubmit={this.handleSubmit}>
-                    <label>Select a time</label><br/>
+                   <label>Select a time: {this.state.timeSlot}</label><br/>
                     <select value= {this.state.timeSlot} onChange={this.handleTimeChange}>
-                        <option default disabled>Select a time slot</option>
-                        {timeSlotArr.map(({time_slot}, index) => <option key={time_slot} time_slot ={time_slot} >{time_slot}</option>)}
+                       <option default disabled>Select a time slot</option>
+                        {this.state.timeSlotArr.map(({time_slot}, index) => <option key={time_slot} time_slot ={time_slot} >{time_slot}</option>)}
                     </select><br/>
-                    <label>Select a dentistry</label><br/>
+                    <label>Select a dentistry: {this.state.dentistry}</label><br/>
                     <select value= {this.state.dentistry} onChange={this.handleDentistryChange}>
                         <option default disabled={this.state.dentistry}>Select your dentistry</option>
-                        {dentistArr.map(({name, id}, index) => <option key={id} id ={id}>{name}</option>)}             
+                        {dentistArr.map(({name, id}, index) => <option key={id} id ={id} >{name}</option>)}             
                     </select>
                     <br/>
-                    <input type="submit" value="Submit" disabled={!this.state.timeSlot && !this.state.dentistry} />
+                    <input type="submit" value="Submit" disabled={!this.state.timeSlot} />
                 </form>
             </div>
         )
